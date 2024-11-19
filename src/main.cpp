81b9110428b9404f99bcfcd4cbab6709bf170f72
Ash3002 <ashu3002sharma@gmail.com>
2024-11-19 16:28:36 +0530

    [any message]

--- src/main.cpp
+++ src/main.cpp
@@ -21,19 +21,9 @@ int main(int argc, char *argv[]) {
     if (command == "tokenize") {
         std::string file_contents = read_file_contents(argv[2]);
         
-            for (char c: file_contents) {
-
-                if (c == '(') {
-
-                    std::cout << "LEFT_PAREN ( null" << std::endl;
-
-                } else if (c == ')') {
-
-                    std::cout << "RIGHT_PAREN ) null" << std::endl;
-
-                }
-
-            }
+        if (!file_contents.empty()) {
+            std::cerr << "Scanner not implemented" << std::endl;
+            return 1;
         }
         std::cout << "EOF  null" << std::endl; 
         
